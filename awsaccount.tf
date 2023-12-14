c297184e18df68ae0f4b6a98459b18fab5c17b3f
Murali Reddy <15521347+muralimreddy@users.noreply.github.com>
2023-12-14 16:12:43 -0500

    Update awsaccount.tf

--- awsaccount.tf
+++ awsaccount.tf
@@ -11,14 +11,10 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_organizations_organization" "cloud_org" {
-}
-
 resource "aws_organizations_account" "account" {
   email = var.root_email
   name = var.account_name
   tags = var.tags
-  parent_id = var.org_unit_id == null ? data.aws_organizations_organization.cloud_org.roots[0].id : var.org_unit_id
 }
 
 

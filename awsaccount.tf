491675655a334e45ca6703af280f11a1fd921f49
ramach-aashrithsagar <ramach.aashrithsagar@servicenow.com>
2024-09-02 15:04:17 +0530

    Update awsaccount.tf

--- awsaccount.tf
+++ awsaccount.tf
@@ -8,15 +8,54 @@ terraform {
 }
 
 provider "aws" {
-  region = var.region
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region     = var.region
 }
 
 resource "aws_organizations_account" "account" {
   email = var.root_email
   name = var.account_name
   tags = var.tags
-  close_on_deletion=var.close_delete
-  create_govcloud=var.gov_cloud
+  close_on_deletion = true
+  parent_id = var.parent_id
 }
 
+resource "aws_budgets_budget" "cost" {
+  name  = join("-", ["SN-CAM-Monthly-Budget", aws_organizations_account.account.id])
+  count = var.monthly_budget > 0 ? 1 : 0
+  budget_type  = "COST"
+  limit_amount = var.monthly_budget
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+  cost_filter {
+    name = "LinkedAccount"
+    values = [
+      aws_organizations_account.account.id
+    ]
+  }
 
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = [var.root_email, var.notification_email]
+  }
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 85
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = [var.root_email, var.notification_email]
+  }
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = [var.root_email, var.notification_email]
+  }
+}
